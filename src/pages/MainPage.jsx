280deb71aa61dd583a134bf914aaa980d7bf1fdc
ZZZINU <98758209+ZZZINU@users.noreply.github.com>
2023-11-28 14:09:56 +0900

    Design: 디폴트 프로필 이미지 변경 테스트

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from "react";
-import "../css/MainPage.css";
-import Logo from "../components/Logo";
-import "../css/fonts/font.css";
-import TagBar from "../components/TagBar";
-import HashTag from "../components/HashTag";
-import MainPost from "../components/MainPost";
-import NavBar from "../components/NavBar";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import '../css/MainPage.css';
+import Logo from '../components/Logo';
+import '../css/fonts/font.css';
+import TagBar from '../components/TagBar';
+import HashTag from '../components/HashTag';
+import MainPost from '../components/MainPost';
+import NavBar from '../components/NavBar';
+import axios from 'axios';
 
 function MainPage({ tagnum }) {
   const [hashtag, setHashtag] = useState([]);
@@ -17,17 +17,15 @@ function MainPage({ tagnum }) {
 
   useEffect(() => {
     axios
-      .get("https://2023-my-awards.com/api/hashtag", {
+      .get('https://2023-my-awards.com/api/hashtag', {
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
         withCredentials: true, // 쿠키사용
       })
-      .then((response) => {
+      .then(response => {
         setCategories(
-          response.data.categories.filter(
-            (categories) => categories.id == tagnum
-          )
+          response.data.categories.filter(categories => categories.id == tagnum)
         );
       });
   }, [tagnum]);
@@ -42,7 +40,7 @@ function MainPage({ tagnum }) {
     }
   };
 
-  const handleTagClick = (tagnum) => {
+  const handleTagClick = tagnum => {
     console.log(tagnum);
     setSelectedTag(tagnum);
   };
@@ -50,73 +48,73 @@ function MainPage({ tagnum }) {
   const handleTagValue = (tagnum, value) => {
     switch (tagnum) {
       case 0:
-        value = "select";
+        value = 'select';
         break;
       case 1:
-        value = "best_all";
+        value = 'best_all';
         break;
       case 2:
-        value = "best_movies";
+        value = 'best_movies';
         break;
       case 3:
-        value = "best_dramas";
+        value = 'best_dramas';
         break;
       case 4:
-        value = "best_books";
+        value = 'best_books';
         break;
       case 5:
-        value = "best_music";
+        value = 'best_music';
         break;
       case 6:
-        value = "best_moments";
+        value = 'best_moments';
         break;
       case 7:
-        value = "best_hobbies";
+        value = 'best_hobbies';
         break;
       case 8:
-        value = "best_discoveries";
+        value = 'best_discoveries';
         break;
       case 9:
-        value = "best_habits";
+        value = 'best_habits';
         break;
       case 10:
-        value = "best_sadness";
+        value = 'best_sadness';
         break;
       case 11:
-        value = "best_thoughts";
+        value = 'best_thoughts';
         break;
       case 12:
-        value = "best_failures";
+        value = 'best_failures';
         break;
       case 13:
-        value = "best_regrets";
+        value = 'best_regrets';
         break;
       case 14:
-        value = "best_humor";
+        value = 'best_humor';
         break;
       case 15:
-        value = "best_tears";
+        value = 'best_tears';
         break;
       case 16:
-        value = "best_spending";
+        value = 'best_spending';
         break;
       case 17:
-        value = "best_emotions";
+        value = 'best_emotions';
         break;
       case 18:
-        value = "best_travels";
+        value = 'best_travels';
         break;
       case 19:
-        value = "best_food";
+        value = 'best_food';
         break;
       case 20:
-        value = "best_gifts";
+        value = 'best_gifts';
         break;
       case 21:
-        value = "best_photos";
+        value = 'best_photos';
         break;
       case 22:
-        value = "next_year_me";
+        value = 'next_year_me';
         break;
     }
     setSelectedValue(value);
@@ -126,14 +124,12 @@ function MainPage({ tagnum }) {
   return (
     <div
       style={{
-        backgroundImage: `url(${
-          "./images/main_bg.png"
-        })`,
-        height: "100vh",
-        overflow: "hidden",
+        backgroundImage: `url(${'./images/main_bg.png'})`,
+        height: '100vh',
+        overflow: 'hidden',
       }}
     >
-      <Logo style={{ display: "inline" }} />
+      <Logo style={{ display: 'inline' }} />
       <div id="main_hashtag" onClick={toggleTagBar}>
         <HashTag tagnum={selectedTag} />
       </div>
@@ -142,7 +138,7 @@ function MainPage({ tagnum }) {
       </div>
       <div
         id="main_tagbar"
-        className={tagBarVisible ? "visible" : "hidden"}
+        className={tagBarVisible ? 'visible' : 'hidden'}
         onClick={TagBarClick}
       >
         <TagBar
